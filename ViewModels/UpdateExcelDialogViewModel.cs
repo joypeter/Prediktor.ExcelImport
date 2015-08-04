db2d561f20620d5a2e1cd15bc5cd720010a5dcd1
Lin Ju <ju.lin@prediktor.no>
2015-08-04 10:55:50 +0800

    Add files

---
+++ ViewModels/UpdateExcelDialogViewModel.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Input;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.ViewModel;
+using Prediktor.Carbon.Infrastructure.Definitions;
+using System.Collections.ObjectModel;
+
+namespace Prediktor.ExcelImport.ViewModels
+{
+    public interface IExcelColumn
+    {
+        string Name { get; }
+        uint Col { get; }
+    }
+
+    public class UpdateExcelDialogViewModel : NotificationObject
+    {
+        //private readonly IInteractionService _interactionService;
+
+        public UpdateExcelDialogViewModel()
+        {
+            //_interactionService = interactionService;
+            _isUseCurrentTime = false;
+            _isAppendNewData = false;
+        }
+
+        private bool _isUseCurrentTime;
+        public bool IsUseCurrentTime
+        {
+            get { return _isUseCurrentTime; }
+            set
+            {
+                _isUseCurrentTime = value;
+            }
+        }
+
+        private bool _isAppendNewData;
+        public bool IsAppendNewData
+        {
+            get { return _isAppendNewData; }
+            set
+            {
+                _isAppendNewData = value;
+            }
+        }
+    }
+}
---
+++ Views/UpdateExcelDialog.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System.Windows;
+using Prediktor.ExcelImport.ViewModels;
+
+namespace Prediktor.ExcelImport.Views
+{
+    /// <summary>
+    /// Interaction logic for ExportDialog.xaml
+    /// </summary>
+    public partial class UpdateExcelDialog : Window
+    {
+        public UpdateExcelDialog()
+        {
+            InitializeComponent();
+
+            //DataContext = exportExcelDialogViewModel;
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            DialogResult = true;
+            Close();
+        }
+    }
+}
