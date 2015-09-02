878bcbb2c5b3a49128c58e656af6c1a5ceb063f0
Peter Jin <peter.jin@prediktor.no>
2015-09-02 15:26:11 +0800

    revert back to latest

--- Views/BrowseDialog.xaml.cs
+++ Views/BrowseDialog.xaml.cs
@@ -1,4 +1,19 @@
-﻿using System.Windows;
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Prediktor.Carbon.Infrastructure.Definitions;
+using Prediktor.Carbon.Infrastructure.Behaviors.Implementation;
+using Prediktor.Carbon.Configuration.Views;
 using System.Collections.ObjectModel;
 
 namespace Prediktor.ExcelImport
